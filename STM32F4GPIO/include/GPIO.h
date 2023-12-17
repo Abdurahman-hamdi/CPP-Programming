2fd5febbf792f22213d89882bd687f1929c751b9
Abdulrahman <abdurrahman.hamdi.95@gmail.com>
2023-12-17 22:03:28 +0200

    adding prototype for gpio port iterface

--- STM32F4GPIO/include/GPIO.h
+++ STM32F4GPIO/include/GPIO.h
@@ -1,27 +1,35 @@
 #ifndef __GPIOD__
 #define __GPIOD__
 
+#include"GPIOCFG.hpp"
 #include<cstdint>
 #include<memory>
 #include "stm32f4xx_gpio.h"
 
-
+//prototype for GPIO model
 namespace IO
 {
-	template< GPIO_port T, config_stratgy type>
+	/**/
+	template< GPIO_port T, typename type>
 	class GPIO
 	{
 
 	public:
-		GPIO(std::unique_ptr<type> custom_port):port{std::move(custom_port)}
+		explicit GPIO(std::unique_ptr<type> custom_port):port{std::move(custom_port)}
 			{
 				
 			}
 	
+		GPIO(const GPIO& )=delete;//ports cann't be copied into each other as they are not same
 
+
+		~GPIO()
+		{
+			//TBD
+		}
 	private:	
-		std::unique_ptr<type> port;
-	
+	// a poiter to the object that will perform all the acticvties of conguired port whatever uart, adc..etc
+		std::unique_ptr<type> port; 
 	};
 	
 	
