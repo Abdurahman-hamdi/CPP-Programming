7072be11bd6e6009eb94cabd5584b0854e91a745
Abdulrahman Hamdi <abdurrahman.hamdi.95@gmail.com>
2023-12-18 17:01:25 +0200

    tbd

--- STM32F4GPIO/include/GPIO.h
+++ STM32F4GPIO/include/GPIO.h
@@ -7,8 +7,7 @@
 #include "stm32f4xx_gpio.h"
 
 //prototype for GPIO model
-namespace IO
-{
+
 	/**/
 	template< GPIO_port T, typename type>
 	class GPIO
@@ -16,28 +15,37 @@ namespace IO
 
 	public:
 		explicit GPIO(std::unique_ptr<type> custom_port):port{std::move(custom_port)}
-			{
+		{
 				
-			}
-	
+		}
 		GPIO(const GPIO& )=delete;//ports cann't be copied into each other as they are not same
 		GPIO(GPIO&& )	=delete;//neither to be moved to another port
-
-
 		type& portaccess()
 		{
 			return port;
 		}
+		void 
 		~GPIO()
-			{
+		{
 				//TBD
-			}
+		}
 	private:	
 	// a poiter to the object that will perform all the acticvties of conguired port whatever uart, adc..etc
 		std::unique_ptr<type> port; 
 	};
 	
+	class IO_function
+	{
+		
+		
+		
+	};
 	
-}
+	class IO_ : public IO_function
+	{
+		
+		
+	}
+	
 
 #endif
